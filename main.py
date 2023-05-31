64c43c71295560028b5ebfcd49f276a7b4184420
Ergüncan <erguncan06@gmail.com>
2023-05-31 23:30:45 +0300

    finalized.

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ from models.gb_model import GBModel
 import joblib
 
 # Load and preprocess data
-df = pd.read_csv('data/dataset.csv')
-X, y = preprocess_data('data/dataset.csv')
+X, y = preprocess_data('data/dataseto.csv')
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Check if the model.pkl file exists
-if os.path.exists('model.pkl'):
+if os.path.exists('modelo.pkl'):
     # Load the model from the pickle file
-    best_model = joblib.load('model.pkl')
+    best_model = joblib.load('modelo.pkl')
 else:
     # Define models
     models = [LogRegModel(), RFModel(), GBModel()]
@@ -28,8 +27,8 @@ else:
     best_auc = 0
     for model in models:
         model.train(X_train, y_train)
-        y_pred_proba = model.predict_proba(X_test)[:, 1]  # Change here
-        auc = roc_auc_score(y_test, y_pred_proba)  # And here
+        y_pred_proba = model.predict_proba(X_test)[:, 1]
+        auc = roc_auc_score(y_test, y_pred_proba)
         print(f'Model: {model.__class__.__name__}, AUC: {auc}')
         if auc > best_auc:
             best_model = model
@@ -38,16 +37,19 @@ else:
     print(f'Best model: {best_model.__class__.__name__}, AUC: {best_auc}')
 
     # Save the best model
-    joblib.dump(best_model, 'model.pkl')
+    joblib.dump(best_model, 'modelo.pkl')
 
 # Use the model to predict probabilities for all data
 y_proba = best_model.predict_proba(X)[:, 1]
 
+# Load original data to get customer ids
+customer_id_data = pd.read_csv('customer_id_data.csv')
+
 # Create a new dataframe with customer ids and predicted probabilities
 output_df = pd.DataFrame({
-    'customer_id': df['uuid'],
+    'customer_id': customer_id_data['uuid'],
     'probability': y_proba
 })
 
 # Write the dataframe to a new csv file
-output_df.to_csv('output.csv', index=False)
+output_df.to_csv('outputo.csv', index=False)
--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -7,7 +7,33 @@ import pandas as pd
 
 def preprocess_data(filepath):
     # Load data
-    df = pd.read_csv(filepath)
+    df = pd.read_csv(filepath, delimiter=';')
+
+    # Save customer_id data
+    customer_id_data = df['uuid']
+
+    # Drop unnecessary columns
+    df = df.drop(['worst_status_active_inv', 'avg_payment_span_0_3m', 'account_incoming_debt_vs_paid_0_24m', 'name_in_email', 'uuid'], axis=1)
+
+    # Convert 'has_paid' to binary
+    df['has_paid'] = df['has_paid'].map({'TRUE': 1, 'FALSE': 0})
+
+    # Remove rows with empty 'default' values
+    nan_indices = df[df['default'].isna()].index
+    df = df.drop(nan_indices)
+    customer_id_data = customer_id_data.drop(nan_indices)
+
+    # Replace NA values with 0 for specified columns
+    columns_to_zero_fill = ["account_status", "account_worst_status_0_3m", "account_worst_status_12_24m",
+                            "account_worst_status_3_6m","account_worst_status_6_12m"]
+    df[columns_to_zero_fill] = df[columns_to_zero_fill].fillna(0)
+
+    # Replace NA values with column average for specified columns
+    columns_to_average_fill = ["avg_payment_span_0_12m", "num_arch_written_off_12_24m",
+                               "num_arch_written_off_0_12m", "num_active_div_by_paid_inv_0_12m",
+                               "account_days_in_term_12_24m", "account_days_in_rem_12_24m"]
+    for column in columns_to_average_fill:
+        df[column] = df[column].fillna(df[column].mean())
 
     numeric_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median')),
@@ -17,15 +43,13 @@ def preprocess_data(filepath):
         ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
         ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
-
-
     # Transform data
     y = df['default']
     X = df.drop('default', axis=1)
 
     # Define preprocessing
     numeric_features = X.select_dtypes(include=['int64', 'float64']).columns
-    categorical_features = X.select_dtypes(include=['object']).columns
+    categorical_features = ['merchant_category', 'merchant_group']
 
     preprocessor = ColumnTransformer(
         transformers=[
@@ -33,5 +57,10 @@ def preprocess_data(filepath):
             ('cat', categorical_transformer, categorical_features)])
 
     X = preprocessor.fit_transform(X)
+
+    # Saving customer_id and processed data frame into csv file
+    customer_id_data.to_csv('customer_id_data.csv', index=False)
+    processed_df = pd.DataFrame(X)
+    processed_df.to_csv('processed_dataset.csv', index=False)
 
     return X, y
